7d73f450d423677e215ed198b28f260241120637
Hussein Elgridly <hussein@broadinstitute.org>
2019-11-13 15:03:24 +0000

    tidy

--- deployall.sh
+++ deployall.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+#todo: pass in env vars. see https://cloud.google.com/sdk/gcloud/reference/functions/deploy#--env-vars-file
+
 pushd functions
 gcloud --quiet functions deploy iservice --runtime python37 --timeout 540s --trigger-http
 popd
--- functions/main.py
+++ functions/main.py
@@ -9,6 +9,3 @@ def iservice(request: flask.Request) -> flask.Response:
 # Update this with a list of all HTTP cloud functions, it's used to build the unit testing client
 ALL_HTTP_FUNCTIONS = [iservice]
 
-
-#todo: deploy to cloud function. pass --env-vars-file to get environment-y things in
-#see https://cloud.google.com/sdk/gcloud/reference/functions/deploy#--env-vars-file
