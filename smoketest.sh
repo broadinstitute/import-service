d98a4abb6d89fbce18f5e3f734efd44126bf8fe5
Hussein Elgridly <hussein@broadinstitute.org>
2019-11-15 23:58:08 +0000

    smoke test the message twice, to check for session weirdness

--- smoketest.sh
+++ smoketest.sh
@@ -12,11 +12,13 @@ echo "That last line should look like a UUID. If it doesn't, something's broken!
 
 echo ""
 
-echo "Putting a message on the PubSub queue..."
-PUBLISH_MSG=`gcloud --project broad-dsde-dev pubsub topics publish task_chunk_topic --attribute=job_id="$JOB_ID",boo=fnoo`
+echo "Putting two messages on the PubSub queue..."
+gcloud --project broad-dsde-dev pubsub topics publish task_chunk_topic --attribute=job_id="$JOB_ID",boo=fnoo
+sleep 2
+gcloud --project broad-dsde-dev pubsub topics publish task_chunk_topic --attribute=job_id="$JOB_ID",boo=fnoo
 echo "...ok, done."
 
 echo ""
 
-echo "Now go to the following URL. You should see your UUID in a log at the bottom. It might take a few seconds to show; keep refreshing."
+echo "Now go to the following URL. There should be two logs with your UUID at the bottom. It might take a few seconds to show; keep refreshing."
 echo "https://console.cloud.google.com/logs/viewer?project=broad-dsde-dev&folder&organizationId&resource=cloud_function%2Ffunction_name%2Ftaskchunk&minLogLevel=0&expandAll=false"
