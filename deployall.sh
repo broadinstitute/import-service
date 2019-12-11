da401f5cabf9a317627b18de7f13691684e82db1
Hussein Elgridly <hussein@broadinstitute.org>
2019-12-11 16:18:21 -0500

    commentary

--- deployall.sh
+++ deployall.sh
@@ -1,12 +1,23 @@
 #! /usr/bin/env bash
 
+echo "The following two operations might fail if the topic and subscription already exist. If this happens, it's fine."
 
-# TODO: make it be okay for these two to fail because they already exist
+echo ""
+echo "Making the pub/sub topic..."
+
 gcloud pubsub topics create task_chunk_topic
+
+echo ""
+echo "Making the pub/sub subscription..."
+
 gcloud pubsub subscriptions create task_chunk_subscription \
     --topic task_chunk_topic \
     --push-endpoint \
     "https://import-service-dot-broad-dsde-dev.appspot.com/_ah/push-handlers/receive_messages?token=$(cat token.secret)" \
     --ack-deadline 10
 
+echo ""
+echo ""
+
+echo "Now deploying the app to GAE."
 gcloud app deploy --quiet
