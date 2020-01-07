5991db1584e47a891aed3e282773a103a41f6bed
Hussein Elgridly <hussein@broadinstitute.org>
2020-01-07 14:20:51 -0500

    ok codacy jeez

--- app/db/model.py
+++ app/db/model.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # Mypy gets confused about whether sqlalchemy enum columns are strings or enums, see here:
 # https://github.com/dropbox/sqlalchemy-stubs/issues/114
 # This is the (gross) workaround. Keep an eye on the issue and get rid of it once it's fixed.
-from typing import TYPE_CHECKING, Type, TypeVar, Any
+from typing import TYPE_CHECKING, Type, TypeVar
 if TYPE_CHECKING:
     from sqlalchemy.sql.type_api import TypeEngine
     T = TypeVar('T')
