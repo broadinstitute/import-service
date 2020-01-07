784f4ef6faa158c5c482e8881a2b079429823c8e
Matt Bemis <mbemis@broadinstitute.org>
2020-01-07 14:56:31 -0500

    test cleanup

--- app/tests/test_status.py
+++ app/tests/test_status.py
@@ -1,19 +1,8 @@
-import flask.testing
-import json
-import jsonschema
 import pytest
 
-from app.tests import testutils
-from app import service, status
-from app.util import exceptions
-from app.db import db
 from app.auth import userinfo
 from app.db.model import *
-
-
-def test_schema_valid():
-    jsonschema.Draft7Validator.check_schema(service.NEW_IMPORT_SCHEMA)
-
+from app.tests import testutils
 
 good_json = {"path": "foo", "filetype": "pfb"}
 good_headers = {"Authorization": "Bearer ya29.blahblah"}
@@ -33,51 +22,27 @@ pubsub_publish = testutils.fxpatch(
 
 @pytest.mark.usefixtures(sam_valid_user, user_has_ws_access, pubsub_publish, "pubsub_fake_env")
 def test_get_one(client):
-    resp = client.post('/iservice/namespace/name/import', json=good_json, headers=good_headers)
+    import_id = client.post('/iservice/namespace/name/import', json=good_json, headers=good_headers).get_data(as_text=True)
+
+    resp = client.get('/iservice/namespace/name/import/{}'.format(import_id), headers=good_headers)
     assert resp.status_code == 200
-
-    # response contains the job ID, check it's actually in the database
-    sess = db.get_session()
-    dbres = sess.query(Import).filter(Import.id == resp.get_data(as_text=True)).all()
-    import_id = dbres[0].id
-    assert len(dbres) == 1
-    assert dbres[0].id == str(resp.get_data(as_text=True))
-
-    resp2 = client.get('/iservice/namespace/name/import/{}'.format(resp.get_data(as_text=True)), headers=good_headers)
-    assert resp2.status_code == 200
-    assert resp2.get_data(as_text=True) == str({"id": import_id, "status": ImportStatus.Pending.name})
+    assert resp.get_data(as_text=True) == str({"id": import_id, "status": ImportStatus.Pending.name})
 
 
 @pytest.mark.usefixtures(sam_valid_user, user_has_ws_access, pubsub_publish, "pubsub_fake_env")
 def test_get_all(client):
-    resp = client.post('/iservice/namespace/name/import', json=good_json, headers=good_headers)
+    import_id = client.post('/iservice/namespace/name/import', json=good_json, headers=good_headers).get_data(as_text=True)
+
+    resp = client.get('/iservice/namespace/name/import', headers=good_headers)
     assert resp.status_code == 200
-
-    # response contains the job ID, check it's actually in the database
-    sess = db.get_session()
-    dbres = sess.query(Import).filter(Import.id == resp.get_data(as_text=True)).all()
-    import_id = dbres[0].id
-    assert len(dbres) == 1
-    assert import_id == str(resp.get_data(as_text=True))
-
-    resp2 = client.get('/iservice/namespace/name/import', headers=good_headers)
-    assert resp2.status_code == 200
-    assert resp2.get_data(as_text=True) == str([{"id": import_id, "status": ImportStatus.Pending.name}])
+    assert resp.get_data(as_text=True) == str([{"id": import_id, "status": ImportStatus.Pending.name}])
 
 
 @pytest.mark.usefixtures(sam_valid_user, user_has_ws_access, pubsub_publish, "pubsub_fake_env")
 def test_get_all_running(client):
-    resp = client.post('/iservice/namespace/name/import', json=good_json, headers=good_headers)
+    client.post('/iservice/namespace/name/import', json=good_json, headers=good_headers)
+
+    resp = client.get('/iservice/namespace/name/import?running_only', headers=good_headers)
     assert resp.status_code == 200
-
-    # response contains the job ID, check it's actually in the database
-    sess = db.get_session()
-    dbres = sess.query(Import).filter(Import.id == resp.get_data(as_text=True)).all()
-    import_id = dbres[0].id
-    assert len(dbres) == 1
-    assert import_id == str(resp.get_data(as_text=True))
-
-    resp2 = client.get('/iservice/namespace/name/import?running_only', headers=good_headers)
-    assert resp2.status_code == 200
-    assert resp2.get_data(as_text=True) == str([])
+    assert resp.get_data(as_text=True) == str([])
 
