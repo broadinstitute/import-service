0382a00faf09b2743cf0c55b56e7c6d6e0453a47
Matt Bemis <mbemis@broadinstitute.org>
2020-01-09 11:09:20 -0500

    more pr feedback addressed

--- app/status.py
+++ app/status.py
@@ -19,7 +19,10 @@ def handle_get_import_status(request: flask.Request, ws_ns: str, ws_name: str, i
 
     try:
         with db.session_ctx() as sess:
-            imprt = sess.query(model.Import).filter(model.Import.id == import_id).one()
+            imprt = sess.query(model.Import).\
+                filter(model.Import.workspace_namespace == ws_ns).\
+                filter(model.Import.workspace_name == ws_name).\
+                filter(model.Import.id == import_id).one()
             return flask.make_response((json.dumps({"id": imprt.id, "status": imprt.status.name}), 200))
     except NoResultFound:
         return flask.make_response(f"Import {import_id} either does not exist or you do not have access to view it", 404)
@@ -35,9 +38,11 @@ def handle_list_import_status(request: flask.Request, ws_ns: str, ws_name: str) 
     user_auth.workspace_uuid_with_auth(ws_ns, ws_name, access_token, "read")
 
     with db.session_ctx() as sess:
-        q = sess.query(model.Import).filter(model.Import.workspace_namespace == ws_ns).filter(model.Import.workspace_name == ws_name)
+        q = sess.query(model.Import).\
+            filter(model.Import.workspace_namespace == ws_ns).\
+            filter(model.Import.workspace_name == ws_name)
         q = q.filter(model.Import.status == ImportStatus.Running) if running_only else q
-        import_list = q.order_by(model.Import.submit_time).all()
+        import_list = q.order_by(model.Import.submit_time.desc()).all()
         import_statuses = [{"id": imprt.id, "status": imprt.status.name} for imprt in import_list]
 
         return flask.make_response((json.dumps(import_statuses), 200))
