f3811e095bd8e4041ebd35da31c66120b4a7145c
David An <davidan@broadinstitute.org>
2020-01-10 09:41:08 -0500

    streamline audit log test

--- app/tests/test_path_validation.py
+++ app/tests/test_path_validation.py
@@ -86,9 +86,6 @@ def test_audit_logging(client: flask.testing.FlaskClient, caplog):
     resp = client.post('/iservice/namespace/name/import', json=payload, headers=good_headers)
     assert resp.status_code == 400
 
-    basiclog = filter(lambda rec: rec.message == "Unrecognized netloc for PFB import: illegal.domains from https://illegal.domains/should/be/logged", caplog.records)
-    assert len(list(basiclog)) == 1
-
     # if the sam_valid_user fixture changes, this assertion will also need to change
-    auditlog = filter(lambda rec: rec.message == "User 123456 hello@bees.com attempted to import from path https://illegal.domains/should/be/logged", caplog.records)
-    assert len(list(auditlog)) == 1
+    auditlog = filter(lambda rec: rec.message == "UserX 123456 hello@bees.com attempted to import from path https://illegal.domains/should/be/logged", caplog.records)
+    assert list(auditlog), "Expected audit log message to exist if user specified illegal domain; did not find such message in log."
