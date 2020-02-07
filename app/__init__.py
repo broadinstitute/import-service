e25cf6ff8bc76af8f6c657f55ff92181f2c06461
Hussein Elgridly <hussein@broadinstitute.org>
2020-02-07 16:17:10 -0500

    remove gzip, it's handled by GAE and that's not what we meant anyway

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,9 @@
 import flask
-import flask_compress
 from app.server import routes, json_response
-
-compress = flask_compress.Compress()
 
 
 def create_app() -> flask.Flask:
     app = flask.Flask(__name__)
     app.response_class = json_response.JsonResponse
     app.register_blueprint(routes.routes)
-    compress.init_app(app)
     return app
