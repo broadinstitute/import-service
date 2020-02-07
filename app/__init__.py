9a1329b2b2cf679ff9aa6a64873b115d2342ca1e
Hussein Elgridly <hussein@broadinstitute.org>
2020-02-07 13:33:58 -0500

    gzip

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 import flask
+import flask_compress
 from app.server import routes
 
 
 def create_app() -> flask.Flask:
     app = flask.Flask(__name__)
     app.register_blueprint(routes.routes)
+    flask_compress.Compress(app)
     return app
