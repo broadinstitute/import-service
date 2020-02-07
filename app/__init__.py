fcc06bcdc3b56442e237f96f4da70bf856502a27
Hussein Elgridly <hussein@broadinstitute.org>
2020-02-07 14:37:15 -0500

    test gz compression

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ import flask
 import flask_compress
 from app.server import routes, json_response
 
+compress = flask_compress.Compress()
+
 
 def create_app() -> flask.Flask:
     app = flask.Flask(__name__)
     app.response_class = json_response.JsonResponse
     app.register_blueprint(routes.routes)
-    flask_compress.Compress(app)
+    compress.init_app(app)
     return app
