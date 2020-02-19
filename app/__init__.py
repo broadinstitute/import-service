6c6eb22af892b854b538a91304b1ae13c42985ce
Hussein Elgridly <hussein@broadinstitute.org>
2020-02-19 15:15:12 -0500

    disable x-fields swagger thing

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,5 @@ from app.server import routes
 def create_app() -> flask.Flask:
     app = flask.Flask(__name__)
     app.register_blueprint(routes.routes)
+    app.config["RESTX_MASK_SWAGGER"] = False  # disable X-Fields header in swagger
     return app
