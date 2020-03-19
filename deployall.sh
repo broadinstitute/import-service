7ea1c0670b383e624335ad295b8fc12507f89317
David An <davidan@broadinstitute.org>
2020-03-19 17:38:32 -0400

    deploy script should match app.yaml values (#31)

--- deployall.sh
+++ deployall.sh
@@ -1,29 +1,29 @@
 #! /usr/bin/env bash
 
-echo "The following two operations might fail if the topic and subscription already exist. If this happens, it's fine."
+echo "You must have a file named token.secret in this directory, containing the PUBSUB_TOKEN from app.yaml"
+
+echo "The following three operations might fail if the topic and subscription already exist. If this happens, it's fine."
 
 echo ""
 echo "Making the pub/sub topic..."
 
-gcloud pubsub topics create import-service-notify
+gcloud pubsub topics create import-service-notify-dev
 
 echo ""
 echo "Making the pub/sub subscription..."
 
 
-gcloud pubsub subscriptions create import-service-notify-subscription \
-    --topic import-service-notify \
-    --push-endpoint \
-    "https://import-service-dot-broad-dsde-dev.appspot.com/_ah/push-handlers/receive_messages?token=$(cat token.secret)" \
+gcloud pubsub subscriptions create import-service-notify-dev-subscription \
+    --topic import-service-notify-dev \
+    --push-endpoint "https://import-service-dot-broad-dsde-dev.appspot.com/_ah/push-handlers/receive_messages?token=$(cat token.secret)" \
     --ack-deadline 600 \
     --push-auth-service-account="import-service@broad-dsde-dev.iam.gserviceaccount.com" \
     --push-auth-token-audience="importservice.dev.test.firecloud.org"
 
 
 # Pull subscription for FiaBs.
-gcloud pubsub subscriptions create import-service-notify-pull \
-    --topic import-service-notify \
-    "https://import-service-dot-broad-dsde-dev.appspot.com/_ah/push-handlers/receive_messages?token=$(cat token.secret)" \
+gcloud pubsub subscriptions create import-service-notify-dev-pull \
+    --topic import-service-notify-dev \
     --ack-deadline 600
 
 
