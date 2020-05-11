b1228fa0f18c7f4b20d943d5263cd2f274acbb1b
ansingh7115 <singh.an@husky.neu.edu>
2020-05-11 19:37:01 -0400

    review suggestions complete

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,12 @@
 FROM python:3.7.4
 
 RUN apt-get update -y && \
-    apt-get install -y -qq --no-install-recommends wget tar build-essential openssh-client python-openssl python-pip python-dev && \
-    apt-get clean
+    apt-get install -y python-pip python-dev
 
-COPY requirements.txt /app/requirements.txt
+# We copy just the requirements.txt first to leverage Docker cache
+COPY ./requirements.txt /app/requirements.txt
 
-ENV HOME /
-ENV CLOUDSDK_PYTHON_SITEPACKAGES 1
-RUN wget -O google-cloud-sdk.tar.gz https://storage.googleapis.com/cloud-sdk-release/google-cloud-sdk-291.0.0-linux-x86.tar.gz \
-    && tar -xzf google-cloud-sdk.tar.gz \
-    && google-cloud-sdk/install.sh \
-        --usage-reporting=true --path-update=true --bash-completion=true --rc-path=/.bashrc --additional-components \
-        app-engine-python app
-
-RUN mkdir /.ssh
-ENV PATH /google-cloud-sdk/bin:$PATH
-VOLUME ["/.config"]
-
-WORKDIR app/
+WORKDIR /app
 
 RUN pip install -r requirements.txt
 
--- deploy.sh
+++ deploy.sh
@@ -38,7 +38,6 @@ echo "Deploying branch '${GIT_BRANCH}' to ${ENVIRONMENT}"
 set -x
 
 GOOGLE_PROJECT=terra-importservice-${ENVIRONMENT}
-IMPORT_SERVICE_IMAGE=quay.io/broadinstitute/import-service:${GIT_BRANCH}
 
 # Get the key for the service account used for deploying
 docker run --rm -e VAULT_TOKEN=${VAULT_TOKEN} broadinstitute/dsde-toolbox vault read --format=json "secret/dsde/firecloud/$ENVIRONMENT/import-service/deployer.json" | jq .data > deployer.json
@@ -48,7 +47,6 @@ if [ ! -s deployer.json ]; then
     exit 1
 fi
 
-docker pull ${IMPORT_SERVICE_IMAGE}
 
 export DSDE_TOOLBOX_DOCKER_IMG=broadinstitute/dsde-toolbox:consul-0.20.0
 docker pull $DSDE_TOOLBOX_DOCKER_IMG
@@ -62,13 +60,17 @@ docker run -v $PWD:/app \
   -e ENVIRONMENT=${ENVIRONMENT} \
    $DSDE_TOOLBOX_DOCKER_IMG render-templates.sh
 
+export CLOUD_SDK_DOCKER_IMG=gcr.io/google.com/cloudsdktool/cloud-sdk:latest
+docker pull $CLOUD_SDK_DOCKER_IMG
+
+
 # Deploy the app to the specified project
 # If this deploy fails with the following error message:
 #    ERROR: (gcloud.app.deploy) Permissions error fetching application [apps/$GOOGLE_PROJECT]. Please make sure you are using the correct project ID and that you have permission to view applications on the project.
 # This could be due to the service account described by deployer.json either being destroyed and re-created in the project.
-docker run -v $PWD/app.yaml:/app/app.yaml \
-  -v $PWD/deployer.json:/app/deployer.json \
+docker run -v $PWD:/app \
   -e GOOGLE_PROJECT=${GOOGLE_PROJECT} \
+  -w /app \
   --entrypoint "/bin/bash" \
-  ${IMPORT_SERVICE_IMAGE} \
-  -c "gcloud auth activate-service-account --key-file=deployer.json --project=$GOOGLE_PROJECT && gcloud -q app deploy app.yaml --project=$GOOGLE_PROJECT"
+   $CLOUD_SDK_DOCKER_IMG \
+  -c "ls app && gcloud auth activate-service-account --key-file=deployer.json --project=$GOOGLE_PROJECT && gcloud app deploy app.yaml --project=$GOOGLE_PROJECT"
