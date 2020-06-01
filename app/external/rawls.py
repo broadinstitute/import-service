eb7c840dd63df1fa428df2b873c11c46ac70708b
ansingh7115 <singh.an@husky.neu.edu>
2020-06-01 17:10:48 -0400

    attempt prepared statement

--- app/external/rawls.py
+++ app/external/rawls.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import requests
+from requests import Request, Session
 
 from app.util.exceptions import ISvcException
 import urllib
@@ -10,11 +11,14 @@ def encode(param: str) -> str:
     return urllib.parse.quote(param)
 
 def get_workspace_uuid(workspace_namespace: str, workspace_name: str, bearer_token: str) -> str:
+    s = Session()
     encoded_workspace_namespace = encode(workspace_namespace)
     encoded_workspace_name = encode(workspace_name)
-    resp = requests.get(
-        f"{os.environ.get('RAWLS_URL')}/api/workspaces/{encoded_workspace_namespace}/{encoded_workspace_name}?fields=workspace.workspaceId",
-        headers={"Authorization": bearer_token})
+    url = f"{os.environ.get('RAWLS_URL')}/api/workspaces/{encoded_workspace_namespace}/{encoded_workspace_name}?fields=workspace.workspaceId"
+    req = Request('GET', url, headers={"Authorization": bearer_token})
+    prepped = s.prepare_request(req)
+    prepped.url = url
+    resp = s.send(prepped)
 
     if resp.ok:
         return resp.json()["workspace"]["workspaceId"]
@@ -22,6 +26,7 @@ def get_workspace_uuid(workspace_namespace: str, workspace_name: str, bearer_tok
         # just pass the error upwards
         logging.info(f"Got {resp.status_code} from Rawls for {workspace_namespace}/{workspace_name}: {resp.text}")
         raise ISvcException(resp.text, resp.status_code)
+
 
 
 def check_workspace_iam_action(workspace_namespace: str, workspace_name: str, action: str, bearer_token: str) -> bool:
