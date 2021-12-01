5b9987c91e1e3689987f37a56363b24bf3de94cc
David An <davidan@broadinstitute.org>
2021-12-01 10:13:21 -0500

    ignore warnings in translators init

--- app/translators/__init__.py
+++ app/translators/__init__.py
@@ -1,3 +1,7 @@
 from app.translators.translator import Translator
 from app.translators.pfb_to_rawls import PFBToRawls
 from app.translators.parquet_to_rawls import ParquetToRawls
+
+# to ignore "imported but unused" errors from codacy/pyflakes
+import warnings
+warnings.filterwarnings("ignore")
