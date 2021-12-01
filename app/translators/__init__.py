2f10510886e8cf51b52cfbce22e04c627fd619a1
David An <davidan@broadinstitute.org>
2021-12-01 10:57:16 -0500

    flake8?

--- app/translators/__init__.py
+++ app/translators/__init__.py
@@ -1,5 +1,5 @@
 from app.translators.translator import Translator
 from app.translators.pfb_to_rawls import PFBToRawls
-from app.translators.parquet_to_rawls import ParquetToRawls  # noqa: F401
+from app.translators.parquet_to_rawls import ParquetToRawls  # flake8: noqa: F401
 
 # the "noqa: F401" annotations tell pyflakes to ignore "imported but unused" SCA errors
