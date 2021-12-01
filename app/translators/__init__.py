babcd008f0082f04d9cfe40f4ec21713aa43eb6f
David An <davidan@broadinstitute.org>
2021-12-01 11:23:45 -0500

    ignore the whole file?

--- app/translators/__init__.py
+++ app/translators/__init__.py
@@ -1,5 +1,4 @@
+# flake8: noqa
 from app.translators.translator import Translator
 from app.translators.pfb_to_rawls import PFBToRawls
-from app.translators.parquet_to_rawls import ParquetToRawls  # noqa
-
-# the "noqa" annotation tells Prospector/pyflakes to ignore SCA errors
+from app.translators.parquet_to_rawls import ParquetToRawls
