8ad93b6faa5e391bae61ec1e8888ac2cd1b6ec4a
David An <davidan@broadinstitute.org>
2021-12-08 11:52:19 -0500

    single quotes

--- app/translators/tdr_manifest_to_rawls.py
+++ app/translators/tdr_manifest_to_rawls.py
@@ -10,40 +10,40 @@ from app.translators.translator import Translator
 
 class TDRManifestToRawls(Translator):
     def __init__(self, options=None):
-        """Translator for Parquet files."""
+        '''Translator for Parquet files.'''
         if options is None:
             options = {}
-        # TODO AS-1037: set defaults for "create references", "use PK", etc?
+        # TODO AS-1037: set defaults for 'create references', 'use PK', etc?
         defaults = {}
         self.options = {**defaults, **options}
 
     def translate(self, file_like, file_type) -> Iterator[Entity]:
-        logging.info(f"executing a TDRManifestToRawls translation for {file_type}: {file_like}")
+        logging.info(f'executing a TDRManifestToRawls translation for {file_type}: {file_like}')
         # read and parse entire manifest file
         jso = json.load(file_like)
 
-        snapshot = jso["snapshot"] # the snapshot model: table names, primary keys, relationships
-        format = jso["format"]["parquet"]["location"] # the parquet export files
+        snapshot = jso['snapshot'] # the snapshot model: table names, primary keys, relationships
+        format = jso['format']['parquet']['location'] # the parquet export files
 
         # build dict of table->parquet files for the exports
-        exports = dict(map(lambda e: (e["name"], e["paths"]), format["tables"]))
+        exports = dict(map(lambda e: (e['name'], e['paths']), format['tables']))
 
         recs = []
         ops = []
 
         # for each table in the snapshot model, extract the table name and primary key
-        for t in snapshot["tables"]:
-            table_name = t["name"]
+        for t in snapshot['tables']:
+            table_name = t['name']
 
-            tdr_pk = t["primaryKey"]
+            tdr_pk = t['primaryKey']
             if tdr_pk is None:
-                pk = "datarepo_row_id"
+                pk = 'datarepo_row_id'
             elif not isinstance(tdr_pk, list):
                 pk = tdr_pk
             elif isinstance(tdr_pk, list) and len(tdr_pk) == 1:
                 pk = tdr_pk[0]
             else:
-                pk = "datarepo_row_id"
+                pk = 'datarepo_row_id'
 
             ops.append(AddUpdateAttribute('tablename', table_name))
             ops.append(AddUpdateAttribute('primarykey', pk))
