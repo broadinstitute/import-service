cf9d136761196799d1adcb8918197c2acb6a7247
David An <davidan@broadinstitute.org>
2021-12-16 09:58:47 -0500

    usable names

--- app/translators/tdr_manifest_to_rawls.py
+++ app/translators/tdr_manifest_to_rawls.py
@@ -37,34 +37,22 @@ class TDRManifestToRawls(Translator):
         return itertools.chain(*self.translate_tables(import_details, tables))
 
     def translate_tables(self, import_details: Import, tables: List[TDRTable]) -> Iterator[Iterator[Entity]]:
-        # for each table in the snapshot model, build the Rawls entities
+        """Converts a list of TDR tables, each of which contain urls to parquet files, to an iterator of Entity objects"""
         for t in tables:
-            # ops = []  # type: List[AttributeOperation]
-            # ops.append(AddUpdateAttribute('tablename', t.name))
-            # ops.append(AddUpdateAttribute('primarykey', t.primary_key))
-
-            # TODO: parse entities out of Parquet files.
-            # get new pet token as necessary, since pet may have expired. May need to pass in the Import object
-            # to this translate method in order to have all the info we need to get a pet.
-            # parse all parquet files for this table into rawls json, add those attrs to the result. See line 59 for
-            # identifying all the parquet files
-            # ops.append(RemoveAttribute('parquetFiles'))
-            # ops.append(CreateAttributeValueList('parquetFiles'))
             for f in t.parquet_files:
                 yield self.translate_parquet_file(import_details, f, t.name, t.primary_key)
-                # with gcs.open_file(import_details.workspace_google_project, 'bucket', 'path', import_details.submitter) as pqfile:
-                # ops.append(AddListMember('parquetFiles', f))
-
-            # recs.append(Entity(name=t.name, entityType='snapshottable', operations=ops))
 
     def translate_parquet_file(self, import_details: Import, filelocation: str, entity_type: str, pk: str) -> Iterator[Entity]:
+        """Converts a parquet file, represented as a url, to an iterator of Entity objects"""
         logging.info(f'{import_details.id} attempting parquet export file {filelocation} ...')
         url = urlparse(filelocation)
         bucket = url.netloc
         path = url.path
+        # TODO: the call to gcs.open_file will get a new pet key each time. This is overly aggressive; we could probably
+        # reuse tokens to reduce API calls to Sam (and thus chances to fail). Ideally, when opening a file if we encounter
+        # an auth error, we'd *then* get a new pet key and retry.
         with gcs.open_file(import_details.workspace_google_project, bucket, path, import_details.submitter) as pqfile:
             return self.convert_parquet_file_to_entities(pqfile, entity_type, pk)
-
 
     def convert_parquet_file_to_entities(self, file_like: IO, entity_type: str, pk: str) -> Iterator[Entity]:
         """Converts single parquet file-like object to an iterator of Entity objects"""
@@ -76,21 +64,27 @@ class TDRManifestToRawls(Translator):
     def translate_data_frame(self, df: pd.DataFrame, column_names: List[str], entity_type: str, pk: str) -> Iterator[Entity]:
         """convert a pandas dataframe - assumed from a Parquet file - to an iterator of Entity objects"""
         for index, row in df.iterrows():
-            ops = self.convert_parquet_row(row, column_names)
+            ops = self.convert_parquet_row(row, column_names, entity_type, pk)
             # TODO: better primary key detection/resilience?
             yield Entity(row[pk], entity_type, list(ops))
     
-    def convert_parquet_row(self, row: pd.Series, column_names: List[str]) -> Iterator[AddUpdateAttribute]:
+    def convert_parquet_row(self, row: pd.Series, column_names: List[str], entity_type: str, pk: str) -> Iterator[AddUpdateAttribute]:
         """convert a single row of a pandas dataframe - assumed from a Parquet file - to an Entity"""
+        # TODO: AS-1041 append import:snapshotid and import:timestamp attributes
         for colname in column_names:
-            yield self.convert_parquet_attr(colname, row[colname])
+            yield self.convert_parquet_attr(colname, row[colname], entity_type, pk)
 
-    def convert_parquet_attr(self, name: str, value: AttributeValue):
+    def convert_parquet_attr(self, name: str, value: AttributeValue, entity_type: str, pk: str):
         """convert a single cell of a pandas dataframe - assumed from a Parquet file - to an AddUpdateAttribute"""
-        # TODO: if this cell should be a reference, create as a EntityReference instead.
+        # TODO: AS-1038 if this cell should be a reference, create as a EntityReference instead.
         # TODO: if this cell is an array, create as RemoveAttribute/CreateAttributeValueList/AddListMember(s) instead
-        if (isinstance(value, (str, int, float, bool))):
-            usable = value
+        if (name != f'{entity_type}_id'):
+            usable_name = name
         else:
-            usable = str(value)
-        return AddUpdateAttribute(name, usable)
+            usable_name = f'import:{name}'
+
+        if (isinstance(value, (str, int, float, bool))):
+            usable_value = value
+        else:
+            usable_value = str(value)
+        return AddUpdateAttribute(usable_name, usable_value)
