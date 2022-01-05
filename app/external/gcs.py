91855d99185dcc5dc13952315d9e0d12e32f19c8
David An <davidan@broadinstitute.org>
2022-01-05 09:29:39 -0500

    reuse pet keys for all Parquet files in a manifest

--- app/external/gcs.py
+++ app/external/gcs.py
@@ -1,5 +1,6 @@
+import logging
 from contextlib import contextmanager
-from typing import IO, Iterator
+from typing import IO, Any, Dict, Iterator
 
 from app.external import sam
 from gcsfs.core import GCSFileSystem
@@ -8,8 +9,13 @@ from gcsfs.core import GCSFileSystem
 # this method is broken out from translate.py to make it easy to mock in unit tests.
 
 @contextmanager
-def open_file(project: str, bucket: str, path: str, submitter: str) -> Iterator[IO]:
-    pet_key = sam.admin_get_pet_key(project, submitter)
-    manifest_fs = GCSFileSystem(project=project, token=pet_key)
+def open_file(project: str, bucket: str, path: str, submitter: str, auth_key: Dict[str, Any] = {}) -> Iterator[IO]:
+    if auth_key:
+        logging.debug(f'using supplied auth key to read {path}')
+    else:
+        logging.debug(f'retrieving auth key from Sam to read {path}')
+        auth_key = sam.admin_get_pet_key(project, submitter)
+
+    manifest_fs = GCSFileSystem(project=project, token=auth_key)
     with manifest_fs.open(f"{bucket}{path}") as response:
         yield response
