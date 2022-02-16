9dd66c01253caa0c326714a89a0643e82fe900f7
Jordan Safer <jordansafer2@gmail.com>
2022-02-16 13:39:59 -0500

    removing log line for pet key

--- app/translators/sync_permissions.py
+++ app/translators/sync_permissions.py
@@ -28,8 +28,6 @@ def sync_permissions(import_details: Import, snapshot_id: str):
     """Get a user's pet token, and use it to sync workspace readers to tdr to give them snapshot read access."""
     # get the proper credentials to call as the user's pet service account
     pet_token = sam.admin_get_pet_auth_header(import_details.workspace_google_project, import_details.submitter)
-    # TODO next line for dev/debug only, remove-
-    logging.info(f"pet token {pet_token}")
 
     # call policy group emails and add them as readers to the snapshot
     policy_group_emails: List[str] = get_policy_group_emails(import_details.workspace_uuid, pet_token)
