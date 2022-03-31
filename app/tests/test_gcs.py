d06942327aaad3af8c13c206b98dd3f4456fc81c
Drew Hite <drew@drewhite.org>
2022-03-31 14:48:35 -0400

    codacy

--- app/tests/test_gcs.py
+++ app/tests/test_gcs.py
@@ -9,9 +9,7 @@ from app.util import exceptions
 
 @patch('gcsfs.core.GCSFileSystem')
 def test_we_throw_exception_if_file_too_big(mock_gcs):
-    """
-    pass a small file limit of 100 bytes and have mock gcs report file is larger, verify exception is thrown
-    """
+    """pass a small file limit of 100 bytes and have mock gcs report file is larger, verify exception is thrown"""
     mock_gcs.du.return_value = 101
     with pytest.raises(exceptions.GcsFileTooLargeException):
         with gcs.open_file('foo', 'bar', 'path', 'user', file_limit_bytes=100, auth_key={'key': 'val'}, gcsfs=mock_gcs):
@@ -20,9 +18,7 @@ def test_we_throw_exception_if_file_too_big(mock_gcs):
 
 @patch('gcsfs.core.GCSFileSystem')
 def test_no_exception_if_file_small_enough(mock_gcs):
-    """
-    file is under size limit
-    """
+    """file is under size limit"""
     mock_gcs.du.return_value = 99
     with gcs.open_file('foo', 'bucket', 'path', 'user', file_limit_bytes=100, auth_key={'key': 'val'}, gcsfs=mock_gcs):
         mock_gcs.open.assert_called_with('bucketpath')
