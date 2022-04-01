1c3c1705a937d5598e573b52eb82fe163367064e
Drew Hite <drew@drewhite.org>
2022-03-31 22:46:09 -0400

    linter

--- app/util/http.py
+++ app/util/http.py
@@ -9,8 +9,8 @@ from app.util.exceptions import FileTooBigToDownlod
 @contextmanager
 def http_as_filelike(url: str, file_limit_bytes: int = 2147483648) -> Iterator[IO]:
     """Open a file over HTTP and return it as a file-like object."""
-    response = requests.head(url)
-    if int(response.headers.get('content-length', 0)) > file_limit_bytes:
+    http_response = requests.head(url)
+    if int(http_response.headers.get('content-length', 0)) > file_limit_bytes:
         raise FileTooBigToDownlod
     rq = urllib.request.Request(url)
     with urllib.request.urlopen(rq) as response:
