1ce5f62273aa64661cd93e4f29497a71a8d9acb8
Drew Hite <drew@drewhite.org>
2022-04-06 17:53:41 -0400

    codacy

--- app/cleanup.py
+++ app/cleanup.py
@@ -6,8 +6,8 @@ from app.db.model import Import, ImportStatus
 
 def clean_up_stale_imports():
     with db.session_ctx() as sess:
-        stuck_jobs = sess.execute("""select id, status from imports where status NOT IN ('Error', 'Done', 'TimedOut') 
-        and HOUR(TIMEDIFF(NOW(), submit_time)) > 36""")
+        stuck_jobs = sess.execute("""select id, status from imports where status NOT IN ('Error', 'Done', 'TimedOut')
+         and HOUR(TIMEDIFF(NOW(), submit_time)) > 36""")
         for job in stuck_jobs:
             logging.warning(f"Job id {job['id']} has status {job['status']} but is more than 36 hours old")
             Import.update_status_exclusively(job['id'], ImportStatus.from_string(job['status']), ImportStatus.TimedOut,
