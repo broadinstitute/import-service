94e9b8dea5b4a612ef1713c2e0773ecb70d1e484
David An <davidan@broadinstitute.org>
2022-05-03 09:36:51 -0400

    beef up unit test, add doc for exemplar data

---
+++ app/tests/resources/data_with_array.md
@@ -0,0 +1,48 @@
+what's in the data_with_array.pfb file?
+
+`pfb show -i app/tests/resources/data_with_array.pfb | jq .`
+
+returns:
+
+```json
+{
+  "id": "HG01101_cram",
+  "name": "submitted_aligned_reads",
+  "object": {
+    "file_format": "BAM",
+    "error_type": "file_size",
+    "file_name": "foo.bam",
+    "file_size": 512,
+    "file_state": [
+      "00000000-0000-0000-0000-000000000000",
+      "11111111-1111-1111-1111-111111111111",
+      "22222222-2222-2222-2222-222222222222"
+    ],
+    "md5sum": "bdf121aadba028d57808101cb4455fa7",
+    "object_id": "dg.4503/cc32d93d-a73c-4d2c-a061-26c0410e74fa",
+    "created_datetime": null,
+    "ga4gh_drs_uri": "drs://example.org/dg.4503/cc32d93d-a73c-4d2c-a061-26c0410e74fa",
+    "participant_id": "bbb1234",
+    "specimen_id": "spec1111",
+    "experimental_strategy": null,
+    "study_registration": "example.com/study_registration",
+    "study_id": "aaa1234",
+    "project_id": "DEV-test",
+    "state": "uploading",
+    "submitter_id": "HG01101_cram",
+    "subject_id": "p1011554-9",
+    "updated_datetime": null
+  },
+  "relations": []
+}
+```
+
+This file is based off the "Example of minimal PFB" in the [pypfb repository](https://github.com/uc-cdis/pypfb#example-of-minimal-pfb), but has been modified to include an array of values in the `file_state` property.
+
+Characteristics of this data:
+    * 16 non-null properties
+    * 3 null properties
+    * the `file_size` property contains a number
+    * the `file_state` property is an arraywith 3 values
+
+Translating this PFB to entity operations should result in 20 total operations (15 AddUpdateAttribute, 1 RemoveAttribute, 1 CreateAttributeValueList, 3 AddListMember)
--- app/tests/test_pfb_to_rawls.py
+++ app/tests/test_pfb_to_rawls.py
@@ -1,9 +1,41 @@
+from app.external.rawls_entity_model import AddListMember, CreateAttributeValueList, RemoveAttribute
 from app.translators.pfb_to_rawls import PFBToRawls
 
 # file-like to ([Entity])
 def test_translate_pfb_file_with_array_to_entities(fake_import, fake_pfb_with_array):
+    # see app/tests/resources/data_with_array.md to understand what's in "fake_pfb_with_array"
 
     translator = PFBToRawls()
+    entities = list(translator.translate(fake_import, fake_pfb_with_array))
 
-    entities = translator.translate(fake_import, fake_pfb_with_array)
-    assert(list(entities) == "hello")
+    array_prop = 'file_state'
+
+    assert len(entities) == 1
+    ent = entities[0]
+    assert ent.name == 'HG01101_cram'
+    assert ent.entityType == 'submitted_aligned_reads'
+    assert len(ent.operations) == 20 # will be 16 prior to respecting arrays
+
+    # we should have 1 RemoveAttribute, 1 CreateAttributeValueList, and 3 AddListMember
+    remove_attribute_ops = filter(lambda op: isinstance(op, RemoveAttribute), ent.operations)
+    assert len(remove_attribute_ops) == 1
+
+    create_list_ops = filter(lambda op: isinstance(op, CreateAttributeValueList), ent.operations)
+    assert len(create_list_ops) == 1
+
+    add_list_member_ops = filter(lambda op: isinstance(op, AddListMember), ent.operations)
+    assert len(create_list_ops) == 3
+
+    # finally, let's validate that these operations occur in the proper order,
+    # that they all work on the 'file_state' property,
+    # and that the AddListMembers have the correct values
+    start_index = ent.operations.index(RemoveAttribute(array_prop))
+    slice = ent.operations[start_index:start_index+4]
+    expected = list(
+        RemoveAttribute(array_prop),
+        CreateAttributeValueList(array_prop),
+        AddListMember(array_prop, '00000000-0000-0000-0000-000000000000'),
+        AddListMember(array_prop, '11111111-1111-1111-1111-111111111111'),
+        AddListMember(array_prop, '22222222-2222-2222-2222-222222222222'))
+    assert slice == expected
+
